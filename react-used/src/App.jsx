0ae119db8df18bb34898ae103a1e659c6045689a
Soroso <tudgus010707@naver.com>
2024-01-27 00:16:09 +0900

    Player 추가

--- react-used/src/App.jsx
+++ react-used/src/App.jsx
@@ -1,9 +1,11 @@
 import Header from "./components/Header";
+import Player from "./components/Player";
 
 function App() {
   return (
     <>
       <Header />
+      <Player />
     </>
   );
 }
--- react-used/src/components/Header.jsx
+++ react-used/src/components/Header.jsx
@@ -5,7 +5,7 @@ export default function Header() {
   return (
     <div className="headerIconContainer bg-black flex justify-between pt-3 pl-3 h-16">
       <div className="headerIcon text-red flex items-center text-3xl">
-        <button>
+        <button className="mr-1">
           <FaYoutube />
         </button>
         <label className="headerMark text-white font-roboto font-bold ">
