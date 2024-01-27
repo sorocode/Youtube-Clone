9c125b3e0df837efbf2bab180b388c374ab46f35
Soroso <tudgus010707@naver.com>
2024-01-27 23:16:53 +0900

    player -> sticky

--- react-used/src/components/Player.jsx
+++ react-used/src/components/Player.jsx
@@ -2,7 +2,7 @@ import ReactPlayer from "react-player";
 
 export default function Player() {
   return (
-    <div className="flex max-w-[1000px] mx-auto">
+    <div className="flex max-w-[1000px] mx-auto sticky">
       <ReactPlayer
         url={"https://youtu.be/yomSywNstWI?si=qi9t9yq5D2WvHM-V"}
         width="100%"
