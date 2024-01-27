3997fc7976ca527a06154255e65315a4bb37c1bf
Soroso <tudgus010707@naver.com>
2024-01-27 10:21:54 +0900

    플레이어 가운데 배치

--- react-used/src/components/Player.jsx
+++ react-used/src/components/Player.jsx
@@ -2,11 +2,11 @@ import ReactPlayer from "react-player";
 
 export default function Player() {
   return (
-    <div className="flex justify-center">
+    <div className="flex max-w-[1000px] mx-auto">
       <ReactPlayer
         url={"https://www.youtube.com/watch?v=7sDY4m8KNLc"}
         width="100%"
-        playing="true"
+        // playing="true"
         controls="true"
       />
     </div>
